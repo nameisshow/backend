7483345ce6bb645d107e19d647c8921432eea162
madl <15565608583@163.com>
2019-11-14 14:04:22 +0800

    '添加主键'

--- mysite/backend/models.py
+++ mysite/backend/models.py
@@ -5,9 +5,10 @@ class Button(models.Model):
     """
     按钮表
     """
+    bid = models.AutoField(verbose_name='ID', primary_key=True)
     name = models.CharField(max_length=32, unique=True, help_text='按钮名')
     event = models.CharField(max_length=32, unique=True, help_text='按钮所对应的js方法名')
-    type = models.CharField(max_length=32, default='', help_text='按钮出发的操作类型')
+    type = models.CharField(max_length=32, default='', help_text='按钮触发的操作类型')
     sort = models.IntegerField(default=0, help_text='排序')
 
 
@@ -15,7 +16,8 @@ class Module(models.Model):
     """
     模块表
     """
-    name = models.CharField(max_length=32, unique=True, help_text='按钮名')
+    mid = models.AutoField(verbose_name='ID', primary_key=True)
+    name = models.CharField(max_length=32, unique=True, help_text='模块名')
     pid = models.IntegerField(default=0, help_text='模块父级id')
     sort = models.IntegerField(default=0, help_text='排序')
     buttons = models.ManyToManyField(Button)
@@ -26,6 +28,7 @@ class Role(models.Model):
     """
     角色表
     """
+    rid = models.AutoField(verbose_name='ID', primary_key=True)
     name = models.CharField(max_length=32, unique=True, help_text='角色名')
     desc = models.CharField(max_length=300, default='', help_text='关于角色的描述')
     modules = models.ManyToManyField(Module)
@@ -38,6 +41,7 @@ class Admin(models.Model):
     """
     管理员表
     """
+    aid = models.AutoField(verbose_name='ID', primary_key=True)
     username = models.CharField(max_length=64, unique=True, help_text='用户名')
     password = models.CharField(max_length=32, help_text='混合盐值的32位md5密码（md5(密码+盐值)）')
     salt = models.CharField(max_length=6, help_text='盐值，6位随机字符串')
